f98326d2709ae769dd31af9fe441a987ee399a03
Pieupi <upietroguedes@gmail.com>
2026-04-24 01:42:25 -0300

    Update script.js

--- Api/dashboard/script.js
+++ Api/dashboard/script.js
@@ -11,3 +11,49 @@ fetch(API)
   .catch(() => {
     document.getElementById("status").innerText = "Erro"
   })
+
+
+let token = ""
+
+async function login(){
+  const user = document.getElementById("user").value
+  const pass = document.getElementById("pass").value
+
+  const res = await fetch("/admin/login", {
+    method:"POST",
+    headers:{"Content-Type":"application/json"},
+    body: JSON.stringify({user, pass})
+  })
+
+  const data = await res.json()
+
+  token = data.token
+  document.getElementById("painel").style.display = "block"
+  loadUsers()
+}
+
+async function loadUsers(){
+  const res = await fetch("/admin/users", {
+    headers:{ Authorization: "Bearer " + token }
+  })
+
+  const data = await res.json()
+  document.getElementById("lista").innerText = JSON.stringify(data, null, 2)
+}
+
+async function criar(){
+  await fetch("/admin/create", {
+    method:"POST",
+    headers:{
+      "Content-Type":"application/json",
+      Authorization:"Bearer "+token
+    },
+    body: JSON.stringify({
+      name:"Novo",
+      plan:"free",
+      limit:50
+    })
+  })
+
+  loadUsers()
+}
