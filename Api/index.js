a8559c9056dc63374fbbcd2f4ec1d7db0461a39c
Pieupi <upietroguedes@gmail.com>
2026-04-24 01:39:52 -0300

    Update index.js

--- Api/index.js
+++ Api/index.js
@@ -3,6 +3,9 @@ import express from "express"
 import dashboard from "./routes/dashboard.js"
 import downloads from "./routes/downloads.js"
 import consultas from "./routes/consultas.js"
+import { auth } from "./middlewares/auth.js"
+import admin from "./routes/admin.js"
+import media from "./routes/media.js"
 
 import { auth } from "./middlewares/auth.js"
 import { rateLimit } from "./middlewares/rateLimit.js"
@@ -12,6 +15,13 @@ const app = express()
 app.use(express.json())
 
 // protege tudo
+
+/ rotas admin (sem apikey)
+app.use("/admin", admin)
+
+// rotas públicas protegidas por apikey
+app.use("/media", auth, media)
+
 app.use(auth)
 
 app.get("/", (req, res) => {
