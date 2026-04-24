934e24033cfa708ebc19cf946534966356866043
Pieupi <upietroguedes@gmail.com>
2026-04-24 01:28:28 -0300

    Update auth.js

--- Api/middlewares/auth.js
+++ Api/middlewares/auth.js
@@ -1,14 +1,43 @@
-const API_KEYS = ["123456", "abcdef"]
+
+import fs from "fs"
+
+const DB_PATH = "./api/database/users.json"
 
 export function auth(req, res, next) {
   const key = req.query.apikey
 
-  if (!key || !API_KEYS.includes(key)) {
+  if (!key) {
     return res.status(401).json({
+      status: false,
+      error: "API KEY obrigatória"
+    })
+  }
+
+  const users = JSON.parse(fs.readFileSync(DB_PATH))
+  const user = users.find(u => u.apikey === key)
+
+  if (!user) {
+    return res.status(403).json({
       status: false,
       error: "API KEY inválida"
     })
   }
+
+  if (user.used >= user.limit) {
+    return res.status(429).json({
+      status: false,
+      error: "Limite atingido"
+    })
+  }
+
+  // aumenta uso
+  user.used += 1
+
+  // salva
+  fs.writeFileSync(DB_PATH, JSON.stringify(users, null, 2))
+
+  // injeta user na req
+  req.user = user
 
   next()
 }
